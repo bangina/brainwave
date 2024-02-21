d4c7aec0106f69994fe3b10776978134ed4629c2
bangina <bbangina@gmail.com>
2024-02-21 09:19:06 +0900

    feat: add Header markup

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,40 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h1>
+        <a href="/" aria-label="Brainwave.io">
+          Brainwave.io
+        </a>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/" aria-label="Demos">
+              Demos
+            </a>
+          </li>
+          <li>
+            <a href="/" aria-label="Demos">
+              Pages
+            </a>
+          </li>
+          |
+          <li>
+            <a href="/" aria-label="Sign In">
+              Sign In
+            </a>
+          </li>
+          <li>
+            <button>
+              <a href="/" aria-label="Sign Up">
+                Sign Up
+              </a>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;

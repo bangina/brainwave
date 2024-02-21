9b8bb7d6aa4da9aa40da6cbc8edd28de7782554e
bangina <bbangina@gmail.com>
2024-02-21 09:11:42 +0900

    feat: add basic color theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      blue: "#473BF0",
+      black: "#161C2D",
+      green: "#68D585",
+      white: "#FFFFFF",
+      lightBlue: "rgba(71, 59, 240, 0.08)",
+    },
     extend: {},
   },
   plugins: [],

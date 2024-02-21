3623d3c3b71d338fa1e75396d6546e784975473e
bangina <bbangina@gmail.com>
2024-02-21 09:36:41 +0900

    feat: add SectionTitle component

--- app/components/PricingListSection.tsx
+++ app/components/PricingListSection.tsx
@@ -1,5 +1,14 @@
+import SectionTitle from "./SectionTitle";
+
 function PricingListSection() {
-  return <div>PricingListSection</div>;
+  return (
+    <section>
+      <SectionTitle
+        title="Pricing & Plans"
+        description="With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within few minutes."
+      />
+    </section>
+  );
 }
 
 export default PricingListSection;
---
+++ app/components/SectionTitle.tsx
@@ -0,0 +1,19 @@
+interface SectionTitleProps {
+  title: string;
+  description: string;
+}
+
+function SectionTitle({ title, description }: SectionTitleProps) {
+  return (
+    <article>
+      <h2 className="text-center font-bold text-[60px] leading-[65px] tracking-[-2px] mb-[44px]">
+        {title}
+      </h2>
+      <p className="text-center text-[19px] leading-[32px] tracking-[-0.2px] mb-[53px]">
+        {description}
+      </p>
+    </article>
+  );
+}
+
+export default SectionTitle;
